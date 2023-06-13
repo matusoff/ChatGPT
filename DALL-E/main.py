7779187aea3d16739ef3819d1b0afae051ea8e16
matusoff <94809104+matusoff@users.noreply.github.com>
2023-06-13 17:03:56 -0400

    Create main.py

---
+++ DALL-E/main.py
@@ -0,0 +1,24 @@
+import requests
+
+KEY = 'your key from openai'
+
+def generate(message):
+    url = 'https://api.openai.com/v1/images/generations'
+
+    headers = {
+        'Authorization': f'Bearer {KEY}',
+        'Content-Type': 'application/json',
+        }
+
+    response = requests.post(url, json={"prompt": message}, headers=headers)
+
+    if response.status_code == 200:
+        result = response.json()
+        print(result)
+    else:
+        print("Error:", response.text)
+
+
+text = 'Porshe plus Tesla'
+
+generate(text)
